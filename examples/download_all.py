a882266cb4a394cafe9d0b990b5dc35486d69f53
Robin Kraft <robin@planet.com>
2016-09-14 21:01:29 -0700

    Refactored a bit to generalize to any search query

--- examples/download_all.py
+++ examples/download_all.py
@@ -1,32 +1,24 @@
 import argparse
 import os
 import requests
-
-# our demo filter that filters by geometry, date and cloud cover
-from demo_filters import redding_reservoir
+import json
 
 ASSET_URL = "https://api.planet.com/data/v1/item-types/{}/items/{}/assets/"
-ASSET_TYPE = 'visual'
-ITEM_TYPE = 'REOrthoTile'
 SEARCH_URL = 'https://api.planet.com/data/v1/quick-search'
 
-# Search API request object
-SEARCH_PAYLOAD = {"item_types": [ITEM_TYPE], "filter": redding_reservoir}
-
-# setup auth
+# set up auth
 SESSION = requests.Session()
 SESSION.auth = (os.environ['PLANET_API_KEY'], '')
 
 
 def run_search(search_request):
     result = SESSION.post(SEARCH_URL, json=search_request)
-
     return [f['id'] for f in result.json()['features']]
 
 
-def simple_activate(item_id):
-    item = SESSION.get(ASSET_URL.format('REOrthoTile', item_id))
-    item_activation_url = item.json()[ASSET_TYPE]["_links"]["activate"]
+def activate(item_id, item_type, asset_type):
+    item = SESSION.get(ASSET_URL.format(item_type, item_id))
+    item_activation_url = item.json()[asset_type]["_links"]["activate"]
 
     print 'Activating visual for {}'.format(item_id)
 
@@ -39,9 +31,9 @@ def simple_activate(item_id):
         return True
 
 
-def check_activation(item_id):
-    item = SESSION.get(ASSET_URL.format('REOrthoTile', item_id))
-    status = item.json()['visual']['status']
+def check_activation(item_id, item_type, asset_type):
+    item = SESSION.get(ASSET_URL.format(item_type, item_id))
+    status = item.json()[asset_type]['status']
     print '{}: {}'.format(item_id, status)
 
     if status == 'active':
@@ -50,7 +42,7 @@ def check_activation(item_id):
         return False
 
 
-def download_image(url, path, item_id):
+def download(url, path, item_id):
     local_path = os.path.join(path, item_id + '.tif')
 
     # memory-efficient download, per
@@ -65,53 +57,74 @@ def download_image(url, path, item_id):
     return True
 
 
+def process_activation(func, search_payload, item_type, asset_type):
+    results = []
+
+    ids = run_search(search_payload)
+
+    for item_id in ids:
+        result = func(item_id, item_type, asset_type)
+        results.append(result)
+
+    return results
+
+
+def process_download(path, search_payload, item_type, asset_type):
+    results = []
+
+    item_ids = run_search(search_payload)
+
+    for item_id in item_ids:
+        item = SESSION.get(ASSET_URL.format(item_type, item_id))
+
+        download_url = item.json()[asset_type]['location']
+        result = download(download_url, path, item_id)
+
+        results.append(result)
+
+    return results
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--search', action="store_true")
     parser.add_argument('--check', action="store_true")
     parser.add_argument('--activate', action="store_true")
-    parser.add_argument('--download')
+    parser.add_argument('--download', help="Path where downloaded files should be stored")
+    parser.add_argument('query', help="Path to json file containing query")
+    parser.add_argument('item', help="Item type (e.g. REOrthoTile or PSOrthoTile")
+    parser.add_argument('asset', help="Asset type (e.g. visual, analytic, analytic_xml")
 
     args = parser.parse_args()
 
+    # load query json file
+    with open(args.query, 'r') as fp:
+        query = json.load(fp)
+
+    # Search API request object
+    search_payload = {"item_types": [args.item], "filter": query}
+
     if args.search:
-        item_ids = run_search(SEARCH_PAYLOAD)
-        print '%d available images' % len(item_ids)
+        results = run_search(search_payload)
+        print '%d available images' % len(results)
 
     if args.activate:
-        results = []
-
-        ids = run_search(SEARCH_PAYLOAD)
-
-        for item_id in ids:
-            result = simple_activate(item_id)
-            results.append(result)
-
+        results = process_activation(activate, search_payload, args.item,
+                                     args.asset)
         msg = 'Requested activation for {} of {} items'
         print msg.format(results.count(True), len(results))
 
+    # check activation status of all data returned by search query
     if args.check:
-        results = []
-
-        ids = run_search(SEARCH_PAYLOAD)
-
-        for item_id in ids:
-            result = check_activation(item_id)
-            results.append(result)
+        results = process_activation(check_activation, search_payload, args.item,
+                                     args.asset)
 
         msg = '{} of {} items are active'
         print msg.format(results.count(True), len(results))
 
+    # download all data returned by search query
     if args.download:
-        path = args.download
-        results = []
-
-        item_ids = run_search(SEARCH_PAYLOAD)
-
-        for item_id in item_ids:
-            item = SESSION.get(ASSET_URL.format('REOrthoTile', item_id))
-            download_url = item.json()['visual']['location']
-            result = download_image(download_url, '/tmp', item_id)
-            results.append(result)
+        results = process_download(args.download, search_payload, args.item,
+                                   args.asset)
         msg = 'Downloaded {} files to {}:'
-        print msg.format(results.count(True), path)
+        print msg.format(results.count(True), args.download)
